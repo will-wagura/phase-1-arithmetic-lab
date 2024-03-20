4701d3c8baa75b1a568c321c0e48a7796b7433e5
William Ndirangu <wwagura0@gmail.com>
2024-03-20 17:19:22 +0300

    Add

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31
+const num2 = 2 
 
-// Write your code here
+const multiply = num1 * num2 
+
+const random = Math.floor(Math.random() * 10) + 1;
+Number.isInteger(random);
+
+const num3 = 8
+const num4 = 4
+
+const mod = num3 - num4 
+
+const number = [15, 16, 17, 18, 19, 20];
+const max = Math.max(...number);
